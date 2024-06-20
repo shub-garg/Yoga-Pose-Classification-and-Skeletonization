f99b8ac0254d2fe27fd878756466f85e11409958
SHUBHAM GARG <shubhamsg123456@gmail.com>
2024-06-20 17:42:48 -0400

    Added vgg16.py

---
+++ models/vgg16.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def VGG16(input_shape=(224, 224, 3), num_classes=1000):
+    model = models.Sequential()
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same', input_shape=input_shape))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(layers.MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(layers.MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(layers.Conv2D(256, (3, 3), activation='relu', padding='same'))
+    model.add(layers.Conv2D(256, (3, 3), activation='relu', padding='same'))
+    model.add(layers.Conv2D(256, (3, 3), activation='relu', padding='same'))
+    model.add(layers.MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(layers.Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(layers.Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(layers.Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(layers.MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(layers.Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(layers.Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(layers.Conv2D(512, (3, 3), activation='relu', padding='same'))
+    model.add(layers.MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(4096, activation='relu'))
+    model.add(layers.Dense(4096, activation='relu'))
+    model.add(layers.Dense(num_classes, activation='softmax'))
+    
+    return model
